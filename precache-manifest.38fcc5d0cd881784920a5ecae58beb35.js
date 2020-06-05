35f567855e34b4c81c3a824e4b89f2a8b04ece2e
Kelley Zhang <kz53@cornell.edu>
2020-06-04 22:07:03 -0400

    Updates

--- precache-manifest.38fcc5d0cd881784920a5ecae58beb35.js
+++ precache-manifest.38fcc5d0cd881784920a5ecae58beb35.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "12e3d76dbba4ba3615cb5ec3ee3740d2",
+    "revision": "d9829c7167857b584299bb43ce4e8bd5",
     "url": "/index.html"
   },
   {
-    "revision": "b3264eefd0b8dcfa629f",
-    "url": "/static/css/main.8b546587.chunk.css"
+    "revision": "f43d11a19e6403faf247",
+    "url": "/static/css/main.4b76afa5.chunk.css"
   },
   {
     "revision": "235d99111acef9081726",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.5e2c5af4.chunk.js.LICENSE"
   },
   {
-    "revision": "b3264eefd0b8dcfa629f",
-    "url": "/static/js/main.add30217.chunk.js"
+    "revision": "f43d11a19e6403faf247",
+    "url": "/static/js/main.57d8e116.chunk.js"
   },
   {
     "revision": "b8d81126cfea448e0658",
